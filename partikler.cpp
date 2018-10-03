648fbd46b39ea7e1dcc8b4324809b10b5df4f900
gregor <gregor.olenik@web.de>
2018-10-03 20:05:08 +0200

    Further minor refactoring

--- partikler.cpp
+++ partikler.cpp
@@ -58,14 +58,23 @@ struct ComputeFacetNormal
 };
 
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     if(GIT_RETRIEVED_STATE) {
-        std::cout << "INFO: " << GIT_HEAD_SHA1 << std::endl;
-        if(GIT_IS_DIRTY) std::cerr << "WARN: there were uncommitted changes." << std::endl;
+        std::cout
+            << "INFO: "
+            << GIT_HEAD_SHA1
+            << std::endl;
+        if(GIT_IS_DIRTY) {
+            std::cerr
+                << "WARN: there were uncommitted changes."
+                << std::endl;
+        }
     }
     else {
-        std::cerr << "WARN: failed to get the current git state. Is this a git repo?" << std::endl;
+        std::cerr
+            << "WARN: failed to get the current git state."
+            << "Is this a git repo?"
+            << std::endl;
         return 0;
     }
 
