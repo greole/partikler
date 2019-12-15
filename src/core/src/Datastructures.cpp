1ea66b9c5899806d35b10f7e7c839099aa066625
Gregor Olenik <gregor.olenik@web.de>
2019-12-15 19:09:33 +0100

    clean up datastructures file

--- src/core/src/Datastructures.cpp
+++ src/core/src/Datastructures.cpp
@@ -19,154 +19,154 @@
 
 #include "Datastructures.hpp"
 
-template <class T>
-void reorder_vector(const std::vector<size_t>& idxs, std::vector<T>& vec) {
-    std::vector<T> tmp(vec.size());
+// template <class T>
+// void reorder_vector(const std::vector<size_t>& idxs, std::vector<T>& vec) {
+//     std::vector<T> tmp(vec.size());
 
-    for(size_t i=0; i<idxs.size(); i++) {
-        tmp[idxs[i]] = vec[i];
-    }
-
-    vec=tmp;
-}
-
-
-// template void reorder_vector<kFacet_handle>;
-template void reorder_vector(const std::vector<size_t>&, std::vector<Facet_handle>& );
-template void reorder_vector(const std::vector<size_t>&, std::vector<size_t>& );
-template void reorder_vector(const std::vector<size_t>&, std::vector<searchcubes::NeighbourPair>& );
-template void reorder_vector(const std::vector<size_t>&, std::vector<VectorPair>& );
-template void reorder_vector(const std::vector<size_t>&, std::vector<STLSurfaceDist>& );
-template void reorder_vector(const std::vector<size_t>&, std::vector<searchcubes::SearchCube>& );
-
-
-// VectorField
-// particle_distance_vec(const PointField &pos, const Field<searchcubes::NeighbourPair> &np) {
-
-//     const size_t ret_size {np.size()};
-
-//     std::vector<Vector> ret(ret_size);
-
-//     OP_NEIGHBOUR_LOOP(const Point &opos = pos[oid];
-//                       const Point &npos = pos[nid];
-//                       const CGALVector lenV = npos - opos;
-
-//                       ret[ctr][0] = lenV[0];
-//                       ret[ctr][1] = lenV[1];
-//                       ret[ctr][2] = lenV[2];)
-
-//         return VectorField(ret, {"tmpx", "tmpy", "tmpz"}, "tmp", true);
-// }
-
-// std::ostream &operator<<(std::ostream &os, FloatField const &m) {
-
-//     auto fs = m.get_field();
-//     os
-//         << "\n"
-//         << m.get_name() << " " << fs.size()
-//         << std::endl;
-
-//     size_t id = 0;
-//     for (auto f : fs) {
-//         os << m.get_name() << "particle " << id << " " << f << "\n" << std::flush;
-//         id++;
+//     for(size_t i=0; i<idxs.size(); i++) {
+//         tmp[idxs[i]] = vec[i];
 //     }
-//     return os;
 
-// }
-
-// std::ostream &operator<<(std::ostream &os, VectorField const &m) {
-
-//     auto fs = m.get_field();
-//     os 
-//         << "\n"
-//         << m.get_name() << " " << fs.size()
-//         << std::endl;
-//     size_t id = 0;
-//     for (auto f : fs) {os
-//             << m.get_name()
-//             << " Particle " << id << " ("
-//             << f[0] << " "
-//             << f[1] << " "
-//             << f[2] << ")\n" << std::flush;
-//         id++;
-//     }
-//     return os;
+//     vec=tmp;
 // }
 
 
+// // template void reorder_vector<kFacet_handle>;
+// template void reorder_vector(const std::vector<size_t>&, std::vector<Facet_handle>& );
+// template void reorder_vector(const std::vector<size_t>&, std::vector<size_t>& );
+// template void reorder_vector(const std::vector<size_t>&, std::vector<searchcubes::NeighbourPair>& );
+// template void reorder_vector(const std::vector<size_t>&, std::vector<VectorPair>& );
+// template void reorder_vector(const std::vector<size_t>&, std::vector<STLSurfaceDist>& );
+// template void reorder_vector(const std::vector<size_t>&, std::vector<searchcubes::SearchCube>& );
 
 
-// std::ostream &operator<<(std::ostream &os, PointField const &m) {
+// // VectorField
+// // particle_distance_vec(const PointField &pos, const Field<searchcubes::NeighbourPair> &np) {
 
-//     auto fs = m.get_field();
-//     os
-//         << "\n"
-//         << m.get_name() << " " << fs.size()
-//         << std::endl;
-//     size_t id = 0;
+// //     const size_t ret_size {np.size()};
+
+// //     std::vector<Vector> ret(ret_size);
+
+// //     OP_NEIGHBOUR_LOOP(const Point &opos = pos[oid];
+// //                       const Point &npos = pos[nid];
+// //                       const CGALVector lenV = npos - opos;
+
+// //                       ret[ctr][0] = lenV[0];
+// //                       ret[ctr][1] = lenV[1];
+// //                       ret[ctr][2] = lenV[2];)
+
+// //         return VectorField(ret, {"tmpx", "tmpy", "tmpz"}, "tmp", true);
+// // }
+
+// // std::ostream &operator<<(std::ostream &os, FloatField const &m) {
+
+// //     auto fs = m.get_field();
+// //     os
+// //         << "\n"
+// //         << m.get_name() << " " << fs.size()
+// //         << std::endl;
+
+// //     size_t id = 0;
+// //     for (auto f : fs) {
+// //         os << m.get_name() << "particle " << id << " " << f << "\n" << std::flush;
+// //         id++;
+// //     }
+// //     return os;
+
+// // }
+
+// // std::ostream &operator<<(std::ostream &os, VectorField const &m) {
+
+// //     auto fs = m.get_field();
+// //     os 
+// //         << "\n"
+// //         << m.get_name() << " " << fs.size()
+// //         << std::endl;
+// //     size_t id = 0;
+// //     for (auto f : fs) {os
+// //             << m.get_name()
+// //             << " Particle " << id << " ("
+// //             << f[0] << " "
+// //             << f[1] << " "
+// //             << f[2] << ")\n" << std::flush;
+// //         id++;
+// //     }
+// //     return os;
+// // }
 
 
-//     for (auto f : fs) {os
-//             << m.get_name()
-//             << " Particle " << id << " ("
-//             << f[0] << " "
-//             << f[1] << " "
-//             << f[2] << ")\n" << std::flush;
-//         id++;
-//     }
-//     return os;
-// }
 
-// void IntField::write_to_disk(std::string path) {
-//     write_int_field(
-//         path,
-//         get_name(),
-//         f_,
-//         get_type(),
-//         "32"
-//         );
-// };
 
-// void SizeTField::write_to_disk(std::string path) {
-//     write_int_field(
-//         path,
-//         get_name(),
-//         f_,
-//         "int",// get_type(),
-//         "32"//"64"
-//         );
+// // std::ostream &operator<<(std::ostream &os, PointField const &m) {
 
-// };
+// //     auto fs = m.get_field();
+// //     os
+// //         << "\n"
+// //         << m.get_name() << " " << fs.size()
+// //         << std::endl;
+// //     size_t id = 0;
 
-// void FloatField::write_to_disk(std::string path) {
-//     write_field(
-//         path,
-//         get_name(),
-//         f_,
-//         get_type(),
-//         "32"
-//         );
-// };
 
-// void VectorField::write_to_disk(std::string path) {
-//     write_vector_field(
-//         path,
-//         get_name(),
-//         f_,
-//         get_type(),
-//         "32",
-//         get_comp_names()
-//         );
-// };
+// //     for (auto f : fs) {os
+// //             << m.get_name()
+// //             << " Particle " << id << " ("
+// //             << f[0] << " "
+// //             << f[1] << " "
+// //             << f[2] << ")\n" << std::flush;
+// //         id++;
+// //     }
+// //     return os;
+// // }
 
-// void PointField::write_to_disk(std::string path) {
-//     write_point_field(
-//         path,
-//         get_name(),
-//         f_,
-//         get_type(),
-//         "32",
-//         get_comp_names()
-//         );
-//         };
+// // void IntField::write_to_disk(std::string path) {
+// //     write_int_field(
+// //         path,
+// //         get_name(),
+// //         f_,
+// //         get_type(),
+// //         "32"
+// //         );
+// // };
+
+// // void SizeTField::write_to_disk(std::string path) {
+// //     write_int_field(
+// //         path,
+// //         get_name(),
+// //         f_,
+// //         "int",// get_type(),
+// //         "32"//"64"
+// //         );
+
+// // };
+
+// // void FloatField::write_to_disk(std::string path) {
+// //     write_field(
+// //         path,
+// //         get_name(),
+// //         f_,
+// //         get_type(),
+// //         "32"
+// //         );
+// // };
+
+// // void VectorField::write_to_disk(std::string path) {
+// //     write_vector_field(
+// //         path,
+// //         get_name(),
+// //         f_,
+// //         get_type(),
+// //         "32",
+// //         get_comp_names()
+// //         );
+// // };
+
+// // void PointField::write_to_disk(std::string path) {
+// //     write_point_field(
+// //         path,
+// //         get_name(),
+// //         f_,
+// //         get_type(),
+// //         "32",
+// //         get_comp_names()
+// //         );
+// //         };
