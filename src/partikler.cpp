466f2d9ba0e57c0105a3c60c753551a3b82b205f
Gregor Olenik <gregor.olenik@web.de>
2020-01-07 12:24:45 +0100

    Pull and build HDF5

--- src/partikler.cpp
+++ src/partikler.cpp
@@ -36,6 +36,7 @@
 #include "yaml-cpp/node/node.h"                // for Node
 #include "yaml-cpp/node/parse.h"               // for LoadFile
 
+#include "H5Cpp.h"
 // Currently under ubuntu the gcc compiler optimises
 // the register to call to the model register away
 // if it doesn't see a call anywere in the executable.
