7b7eb4ce9dd63a2ae76d4d01b9d96ae083b8af97
Gregor Olenik <gregor.olenik@web.de>
2020-01-09 10:03:46 +0100

    Add basic hdf5 and h5hut support, refactored cmake files

--- cmake/build_3rd_party.cmake
+++ cmake/build_3rd_party.cmake
@@ -2,15 +2,18 @@ FUNCTION(BUILD NAME)
     configure_file(cmake/${NAME}.in ${NAME}-download/CMakeLists.txt)
 
     message("CMAKE ${NAME}")
-    execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
       RESULT_VARIABLE result
       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${NAME}-download )
+
     if(result)
       message(FATAL_ERROR "CMake step for ${NAME} failed: ${result}")
     endif()
 
     message("BUILDING ${NAME}")
-    execute_process(COMMAND ${CMAKE_COMMAND} --build .
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} --build .
       RESULT_VARIABLE result
       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${NAME}-download )
     if(result)
---
+++ src/io/src/HDF5Writer.hpp
@@ -0,0 +1,59 @@
+/*  Partikler - A general purpose framework for smoothed particle hydrodynamics
+    simulations Copyright (C) 2019 Gregor Olenik
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    contact: go@hpsim.de
+*/
+
+#ifndef PARTIKLER_HDF5_INCLUDED_H
+#define PARTIKLER_HDF5_INCLUDED_H
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string> // for string
+#include <sys/stat.h>
+
+// #include "H5hut.h"
+#include "Field.hpp"
+#include "Models.hpp"     // for ModelRegister (ptr only), REGISTER_DEC_TYPE
+#include "WriterBase.hpp" // for WriterBase
+#include "cgal/CGALHelper.hpp"
+#include "yaml-cpp/yaml.h"
+
+class ObjectRegistry;
+namespace YAML {
+class Node;
+} // namespace YAML
+
+class HDF5Writer : public WriterBase {
+
+    REGISTER_DEC_TYPE(HDF5Writer);
+
+  private:
+    std::string export_name_;
+
+  public:
+    HDF5Writer(
+        const std::string &model_name,
+        YAML::Node parameter,
+        ObjectRegistry &objReg);
+
+    template <class T> void write_to_disk(T const &t, const std::string path);
+
+    void execute();
+};
+
+#endif
