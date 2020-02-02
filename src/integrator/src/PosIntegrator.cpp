11319ebb0f87f1b98eac87e29983bbf804cc2f85
Gregor Olenik <gregor.olenik@web.de>
2020-02-02 21:37:19 +0100

    Add non stl version of PosIntegrator

---
+++ src/integrator/src/PosIntegrator.cpp
@@ -0,0 +1,63 @@
+/*  Partikler - A general purpose framework for smoothed particle hydrodynamics
+    simulations Copyright (C) 2019 Gregor Olenik
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    contact: go@hpsim.de
+*/
+
+#include "PosIntegrator.hpp"
+
+#include "Time.hpp"
+
+PosIntegrator::PosIntegrator(
+    const std::string &model_name, YAML::Node parameter, ObjectRegistry &objReg)
+
+    : Model(model_name, parameter, objReg),
+      id_(objReg.get_object<IntField>("id")),
+      u_(objReg.get_object<VectorField>("u")),
+      pos_(objReg.get_object<VectorField>("Pos")),
+      time_(objReg.get_object<TimeGraph>("TimeGraph")) {}
+
+void PosIntegrator::execute() {
+
+    log().info_begin() << " Updating Particle Positions";
+    log().info() << " DeltaT " << time_.get_deltaT();
+
+    VectorField dx(u_.size(), {0.0, 0.0, 0.0});
+
+    for (size_t i = 0; i < pos_.size(); i++) {
+        if (id_[i] > 0) {
+            u_[i][0] = 0.0;
+            u_[i][1] = 0.0;
+            u_[i][2] = 0.0;
+        }
+        dx[i] =  u_[i] * time_.get_deltaT();
+    }
+
+    pos_ += dx;
+
+    const float CFL = 0.01;
+    // float current_max_dx = (pos_ - old_pos).norm().get_max();
+    // // float dx_ratio = CFL*dx_max/current_max_dx;
+    // float dx_ratio = CFL /current_max_dx;
+    // float two = 2.0;
+    // float change = min(two, dx_ratio);
+    // time_.set_deltaT(min(time_.get_maxDeltaT(), time_.get_deltaT() *
+    // change));
+
+    log().info_end();
+}
+
+REGISTER_DEF_TYPE(TRANSPORTEQN, PosIntegrator);
