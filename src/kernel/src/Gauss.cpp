78a9729f822e7938e829ebe1225a6bc78b1f5751
Gregor Olenik <gregor.olenik@web.de>
2020-02-04 08:45:20 +0100

    Add Gaussian kernel

---
+++ src/kernel/src/Gauss.cpp
@@ -0,0 +1,110 @@
+/*  Partikler - A general purpose framework for smoothed particle hydrodynamics
+    simulations Copyright (C) 2019 Gregor Olenik
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    contact: go@hpsim.de
+*/
+
+#include "Gauss.hpp"
+
+#include "ObjectRegistry.hpp"
+#include "math.h"
+
+// TODO Implement 2d and 3d kernel by deriving from base class
+
+Gauss::Gauss(
+    const std::string &model_name,
+    YAML::Node parameter,
+    ObjectRegistry &objReg,
+    float hfact)
+    : Model(model_name, parameter, objReg),
+      h_(read_or_default_coeff<Scalar>("h", 1.0)),
+      ih_(1.0 / h_),
+      alpha_(hfact / (h_ * h_)),
+      pos_(objReg.get_pos()),
+      np_(objReg.get_object<Field<std::vector<NeighbourPair>>>(
+          "neighbour_pairs")),
+      W_(objReg.create_field<ScalarField>("KernelW")),
+      dWdx_(objReg.create_field<KernelGradientField>("KerneldWdx")) {}
+
+void Gauss::execute() {
+
+    // log().set_scope("Gauss::execute()");
+    log().info_begin() << "Computing Kernel";
+
+    const size_t size {np_.size()};
+
+    // Resize kernel
+    W_.resize(size);
+    dWdx_.resize(size);
+
+    for (size_t pid = 0; pid < size; pid++) {
+
+        auto [oid, nid] = np_[pid];
+
+        Vec3 const &opos = pos_[oid];
+        Vec3 const &npos = pos_[nid];
+
+        // auto [len, lenVo, lenVn] = sd_[pid];
+
+        const auto len = opos - npos;
+        Scalar const maglen = std::sqrt(squared_length(len));
+
+        const Scalar q {maglen * ih_};
+
+        if (q > 2.) {
+            log().warn() << "Outside kernel radius";
+            W_[pid] = 0.0;
+            dWdx_[pid] = {0, 0, 0};
+            continue;
+        }
+
+        Scalar W = exp(-q * q) * alpha_;
+
+        W_[pid] = W;
+
+        const Scalar prefact = -2.0 * q * W ;
+
+        if (maglen > 0.0) {
+            for (int j = 0; j < 3; j++) {
+                dWdx_[pid][j] = (Scalar)len[j] / maglen * prefact;
+            }
+        } else {
+            for (int j = 0; j < 3; j++) {
+                dWdx_[pid] = {0.0, 0.0, 0.0};
+            }
+            log().warn() << "Neighbour sum == 0";
+        }
+    }
+
+    log().info_end();
+}
+
+Gauss2D::Gauss2D(
+    const std::string &model_name, YAML::Node parameter, ObjectRegistry &objReg)
+    : Gauss(model_name, parameter, objReg, 1.0/M_PI) {}
+
+Gauss3D::Gauss3D(
+    const std::string &model_name, YAML::Node parameter, ObjectRegistry &objReg)
+    : Gauss(
+          model_name,
+          parameter,
+          objReg,
+          1.0 / ((pow(M_PI, 3/2)) *
+                 read_or_default_coeff_impl<Scalar>(parameter, "h", 1.0))
+        ) {}
+
+REGISTER_DEF_TYPE(KERNEL, Gauss2D);
+REGISTER_DEF_TYPE(KERNEL, Gauss3D);
---
+++ src/kernel/src/Gauss.hpp
@@ -0,0 +1,96 @@
+/*  Partikler - A general purpose framework for smoothed particle hydrodynamics
+    simulations Copyright (C) 2019 Gregor Olenik
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    contact: go@hpsim.de
+*/
+
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include <string> // for string
+#include <vector> // for vector
+
+#include "Field.hpp"  // for Field (ptr only), ScalarField, PointField
+#include "Models.hpp" // for ModelRegister (ptr only), REGISTER_DEC_TYPE
+#include "SearchCubes.hpp"
+#include "cgal/CGALHelper.hpp"
+#include "yaml-cpp/yaml.h"
+
+#include "Scalar.hpp"
+
+class ObjectRegistry;
+namespace YAML {
+class Node;
+} // namespace YAML
+struct NeighbourPair;
+struct Vec3;
+
+class Gauss : public Model {
+
+  private:
+    // Coeffs
+    const Scalar h_;  // Smoothing length
+    const Scalar ih_; // Inverse Smoothing length
+    // // 3d
+    // const float W_fak2 = 21. / (256. * M_PI * h * h * h);
+    // const float dW_fak2 = 21. / (256. * M_PI * h * h * h * h);
+    // 2d
+    const Scalar alpha_;  // = 7. / (64. * M_PI * h * h);
+
+    // In
+    const VectorField &pos_; // Particle positions
+
+    const Field<std::vector<NeighbourPair>> &np_;
+    // const Field<STLSurfaceDist> &sd_;
+
+    // Out
+    // Kernel &kernel                               // Kernel field
+    ScalarField &W_;
+    Field<std::vector<Vec3>> &dWdx_;
+
+  public:
+    Gauss(
+        const std::string &model_name,
+        YAML::Node parameter,
+        ObjectRegistry &objReg,
+        float hfact);
+
+    void execute();
+};
+
+class Gauss2D : public Gauss {
+
+    REGISTER_DEC_TYPE(Gauss2D);
+
+  public:
+    Gauss2D(
+        const std::string &model_name,
+        YAML::Node parameter,
+        ObjectRegistry &objReg);
+};
+
+class Gauss3D : public Gauss {
+
+    REGISTER_DEC_TYPE(Gauss3D);
+
+  public:
+    Gauss3D(
+        const std::string &model_name,
+        YAML::Node parameter,
+        ObjectRegistry &objReg);
+};
+
+#endif
